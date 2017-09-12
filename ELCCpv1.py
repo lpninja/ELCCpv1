4b26d3ed08982e0236398b1d7c65d9b3c9fc01c0
Luke <solarengineer@gmail.com>
2017-09-12 13:09:50 +0900

    Updated ELCCv1.py
    Removed water flow sensor, updated comments, and also changed the time format from XX-XX-XX to XX:XX:XX.

--- ELCCpv1.py
+++ ELCCpv1.py
@@ -14,18 +14,22 @@ Comment Component cert in JSON:
   "inverter": "maufacturer modelcode", 
   "data-logger": "maufacturer modelcode",
   "pyranometer": "manufacturer modelcode",
+  """ If there is no pyranometer used, then leave it blank."""
   "windsensor": "manufacturer modelcode",
+  """ If there is no windsensor used, then leave it blank."""
   "rainsensor": "manufacturer modelcode",
-  "waterflow": "manufacturer modelcode",
+  """ If there is no rainsensor used, then leave it blank."""
   "generation": [
-    {"Period1": "20XX-XX-XX-HH-MM-SS-20XX-XX-XX-HH-MM-SS", "MWh": 1.000000}, 
-    {"Period2": "20XX-XX-XX-HH-MM-SS-20XX-XX-XX-HH-MM-SS", "MWh": 1.005000}
+    {"Period1": "20XX-XX-XX-HH:MM:SS-20XX-XX-XX-HH:MM:SS", "MWh": 1.000000}, 
+    {"Period2": "20XX-XX-XX-HH:MM:SS-20XX-XX-XX-HH:MM:SS", "MWh": 1.005000}
     ],
   "Web layer API": "off chain name weblink login",
   "Size kW": X.XXX,
   "lat": "00.000N/S",
+  """For on-chain privacy, your can leave it to less decimals places for Lat/Long"""
   "long": "000.000E/W",
+  """For on-chain privacy, your can leave it to less decimals places for Lat/Long"""
   "Comment": "anything up to 40 characters, The type of IoT running this py script e.g. RPi3b, RPi2 etc.",
    }
 """
-"""Example: {"UserID": "009mx87m543567nqnnbvcdretyupam3mmmwiqw4r","module":"Solarworld Sunmodule Plus SW 265 mono black SW-01-6023US","inverter":"Enphase M250 Microinverter 800-00181-r06","data-logger":"","pyranometer":"","windsensor":"","rainsensor":"","waterflow":"","Web_layer_API":"","Size_kW":"3.975","lat":"51.678N","long":"0.301E","Comment":"any comment up to 40char","RPI 2b, static, solar powered","generation":"2017-04-20-14-10-25-2017-04-20-14-15-27","MWh":"9.046878"}"""
+"""Example: {"UserID": "009mx87m543567nqnnbvcdretyupam3mmmwiqw4r","module":"Solarworld Sunmodule Plus SW 265 mono black SW-01-6023US","inverter":"Enphase M250 Microinverter 800-00181-r06","data-logger":"","pyranometer":"","windsensor":"","rainsensor":"","Web_layer_API":"","Size_kW":"3.975","lat":"51.678N","long":"0.301E","Comment":"any comment up to 40char, RPI 2b, static, solar powered","generation":"2017-04-20-14:10:25-2017-04-20-14:15:27","MWh":"9.046878"}"""
