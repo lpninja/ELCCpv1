1924a77ebcf88f26d5ce38cc6571a769810ac219
Steven Campbell <solarsteve100@gmail.com>
2017-09-25 12:08:00 +0100

    Updated notations and examples

--- ELCCpv1.py
+++ ELCCpv1.py
@@ -8,7 +8,8 @@ __version__ = "1.8"
 """This is the TXID standard. 
 Comment Component cert in JSON: 
 sysv1{
-  "UserID":"SolarCoin affiliate website, or blockpass User ID",
+"""'sys' denotes the following JSON string contains details about the system logging the data, 'v1' allows for version control and changes to future datalogging syntax"""
+  "UID":"SolarCoin affiliate website, or blockpass User ID",
   """The UserID is linked to the appropriate solarcoin affiliate websites UserID, or a third-party service protocol such as blockpass.org, any third party can implement their ID system up to 40 Characters."""
   "module":"manufacturer modelcode",
   "inverter":"maufacturer modelcode", 
@@ -28,17 +29,23 @@ sysv1{
   "Comment":"anything up to 40 characters, The type of IoT running this py script e.g. RPi3b, RPi2, OdroidXU4 etc.",
    }
 """
-"""Example: {"UserID":"009mx87m543567nqnnbvcdretyupam3mmmwiqw4r","module": "Solarworld Sunmodule Plus SW 265 mono black SW-01-6023US","inverter":"Enphase M250 Microinverter 800-00181-r06","data-logger":"","pyranometer":"","windsensor":"","rainsensor":"","Web_layer_API":"","Size_kW":"3.975","lat":"51.678N","long":"0.301E","Comment":"any comment up to 40char, RPI 2b, static, solar powered","generation":"2017-04-20-14:10:25-2017-04-20-14:15:27","MWh":"9.046878"}"""
+"""Example: sysv1{"UID":"7859bc519a67977e06d5e9d58860ab0546f65a29","module":"Solarworld Sunmodule Plus SW 265 mono black SW-01-6023US","inverter":"Enphase M250 Microinverter 800-00181-r06","data-logger":"","pyranometer":"","windsensor":"","rainsensor":"","waterflow":"","Web_layer_API":"","Size_kW":"3.975","lat":"51.678N","long":"0.301E","Comment":"Hello World- I am the first Raspberry Pi Node"}"""
 
 Generation cert in JSON:
-  genv1{"UID":"7859bc519a67977e06d5e9d58860ab0546f65a29",
-        "t0":"2017-09-23 14:42:19","MWh0":"11.339875",
-        "t1":"2017-09-23 16:20:32","MWh1":"11.341858",
-        "t2":"2017-09-23 18:45:24","MWh2":"11.3429",
-        "t3":"2017-09-23 20:56:31","MWh3":"11.3429",
-        "t4":"2017-09-23 23:21:25","MWh4":"11.3429",
-        "t5":"2017-09-24 01:46:19","MWh5":"11.3429",
-        "t6":"2017-09-24 04:11:13","MWh6":"11.3429",
-        "t7":"2017-09-24 06:36:09","MWh7":"11.342917",
-        "t8":"2017-09-24 08:50:08","MWh8":"11.343934",
-        "t9":"2017-09-24 11:15:03","MWh9":"11.348254"}
+  genv1
+"""'gen' denotes the following JSON string contains details of energy generated, 'v1' allows for version control and changes to future datalogging syntax"""
+        {"UID":"7859bc519a67977e06d5e9d58860ab0546f65a29",
+"""The UserID is linked to the appropriate solarcoin affiliate websites UserID, or a third-party service protocol such as blockpass.org, any third party can implement their ID system up to 40 Characters."""
+        "t0":"XXXX-XX-XX XX:XX:XX","MWh0":XXXX.XXXXXX,
+        "t1":"XXXX-XX-XX XX:XX:XX","MWh1":XXXX.XXXXXX,
+        "t2":"XXXX-XX-XX XX:XX:XX","MWh2":XXXX.XXXXXX,
+        "t3":"XXXX-XX-XX XX:XX:XX","MWh3":XXXX.XXXXXX,
+        "t4":"XXXX-XX-XX XX:XX:XX","MWh4":XXXX.XXXXXX,
+        "t5":"XXXX-XX-XX XX:XX:XX","MWh5":XXXX.XXXXXX,
+        "t6":"XXXX-XX-XX XX:XX:XX","MWh6":XXXX.XXXXXX,
+        "t7":"XXXX-XX-XX XX:XX:XX","MWh7":XXXX.XXXXXX,
+        "t8":"XXXX-XX-XX XX:XX:XX","MWh8":XXXX.XXXXXX,
+        "t9":"XXXX-XX-XX XX:XX:XX","MWh9":XXXX.XXXXXX}
+  """'tX' denotes this is a time period, all dates and times in Gregorian calendar, 'MWhX' denotes the system total amount of energy logged
+  at the timestamp, to 11 characters"""
+  Example: genv1{"UID":"7859bc519a67977e06d5e9d58860ab0546f65a29","t0":"2017-09-24 16:13:57","MWh0":11.358824,"t1":"2017-09-24 16:50:12","MWh1":11.358953,"t2":"2017-09-24 17:26:26","MWh2":11.358986,"t3":"2017-09-25 06:43:19","MWh3":11.359,"t4":"2017-09-25 07:19:33","MWh4":11.359062,"t5":"2017-09-25 07:55:46","MWh5":11.359141,"t6":"2017-09-25 08:31:59","MWh6":11.359224,"t7":"2017-09-25 09:08:13","MWh7":11.359468,"t8":"2017-09-25 09:44:26","MWh8":11.359678,"t9":"2017-09-25 10:20:41","MWh9":11.360017}
