07259e3db16fb9eb08b0cb2bd9bdf5a4750c0f23
Steven Campbell <solarsteve100@gmail.com>
2017-10-11 15:42:43 +0100

    Updated examples + Included digital signature protocol
    Included new examples.
    Also included method to 'sign' data-logs, which;
    A: proves the datalogger is not being spoofed, acheived by using SolarCoin wallet message signing + message verification
    B: proves to any user with a copy of datalogger.py that the datalogger making the post to the chain is using an un-tampered copy of datalogger.py by including an SHA1 hash of the datalogger.py program within the signed message

--- ELCCpv1.py
+++ ELCCpv1.py
@@ -11,6 +11,7 @@ sysv1{
 """'sys' denotes the following JSON string contains details about the system logging the data, 'v1' allows for version control and changes to future datalogging syntax"""
   "UID":"SolarCoin affiliate website, or blockpass User ID",
   """The UserID is linked to the appropriate solarcoin affiliate websites UserID, or a third-party service protocol such as blockpass.org, any third party can implement their ID system up to 40 Characters."""
+  "SigAddr":"a new random address selected from the SolarCoin wallet used solely to sign and verify tx-messages"
   "tilt":"tilt angle of the solar panels in degrees to the surface horizontal of the mounting plane, or 'tracked' for systems with tracking.",
   "azimuth":"azimuth angle of the solar panels in degrees (0-359), with 0 be magnetic North in the Northern hemisphere, 180 degrees is magnetic South and 'tracked' for systems with tracking.",
   "module":"manufacturer modelcode",
@@ -30,8 +31,9 @@ sysv1{
   """For on-chain privacy, your can leave it to less decimals places for Lat/Long"""
   "Comment":"anything up to 40 characters, The type of IoT running this py script e.g. RPi3b, RPi2, OdroidXU4 etc.",
    }
+   "Sig": "digital signature based on SigAddr, and using the concatenation of sysv1 message {.....} + SHA1 checksum of the datalogger.py program"
 """
-"""Example: sysv1{"UID":"7859bc519a67977e06d5e9d58860ab0546f65a29","tilt":"40", "azimuth":"180", "module":"Solarworld Sunmodule Plus SW 265 mono black SW-01-6023US","inverter":"Enphase M250 Microinverter 800-00181-r06","data-logger":"","pyranometer":"","windsensor":"","rainsensor":"","waterflow":"","Web_layer_API":"","Size_kW":"3.975","lat":"51.678N","long":"0.301E","Comment":"Hello World- I am the first Raspberry Pi Node"}"""
+"""Example: text:sysv1{"UID":"14dac6fffd768ee96250acd8bd0ff55eae03eae5","SigAddr":"8HZUxJZb2dBmhiMtXhBzNETqBj9NWotxUv","module":"Solarworld Sunmodule Plus SW 265 mono black SW-01-6023US","tilt":"50","azimuth":"210","inverter":"Enphase M250 Microinverter 800-00181-r06","data-logger":"Raspberry Pi2b","pyranometer":"","windsensor":"","rainsensor":"","Web layer API":"","Size_kW":"3.975","lat":"51.678N","long":"0.301E","Comment":"I am the first Raspberry Pi Node"}Sig:IDRMXcpAgHzoVSHFINS0YLx73wtrfMA0AatpbPqdKilQ9kbJM9eAfN3wDpctXpmsewrgGOA2efvpi5fT7gq8h7w="""
 
 Generation cert in JSON:
   genv1
@@ -46,8 +48,7 @@ Generation cert in JSON:
         "t5":"XXXX-XX-XX XX:XX:XX","MWh5":XXXX.XXXXXX,
         "t6":"XXXX-XX-XX XX:XX:XX","MWh6":XXXX.XXXXXX,
         "t7":"XXXX-XX-XX XX:XX:XX","MWh7":XXXX.XXXXXX,
-        "t8":"XXXX-XX-XX XX:XX:XX","MWh8":XXXX.XXXXXX,
-        "t9":"XXXX-XX-XX XX:XX:XX","MWh9":XXXX.XXXXXX}
+        }"Sig":"digital signature based on SigAddr from sysv1 and using the contatenation of genv1 message {.....} + SHA1 checksum of datalogger.py program"
   """'tX' denotes this is a time period, all dates and times in Gregorian calendar, 'MWhX' denotes the system total amount of energy logged
   at the timestamp, to 11 characters"""
-  Example: genv1{"UID":"7859bc519a67977e06d5e9d58860ab0546f65a29","t0":"2017-09-24 16:13:57","MWh0":11.358824,"t1":"2017-09-24 16:50:12","MWh1":11.358953,"t2":"2017-09-24 17:26:26","MWh2":11.358986,"t3":"2017-09-25 06:43:19","MWh3":11.359,"t4":"2017-09-25 07:19:33","MWh4":11.359062,"t5":"2017-09-25 07:55:46","MWh5":11.359141,"t6":"2017-09-25 08:31:59","MWh6":11.359224,"t7":"2017-09-25 09:08:13","MWh7":11.359468,"t8":"2017-09-25 09:44:26","MWh8":11.359678,"t9":"2017-09-25 10:20:41","MWh9":11.360017}
+  Example: genv1{"UID":"14dac6fffd768ee96250acd8bd0ff55eae03eae5","t0":"2017-10-09 15:00:30","MWh0":11.490584,"t1":"2017-10-09 15:15:34","MWh1":11.490653,"t2":"2017-10-09 15:31:07","MWh2":11.490673,"t3":"2017-10-09 15:45:40","MWh3":11.490693,"t4":"2017-10-09 16:00:14","MWh4":11.49071,"t5":"2017-10-09 16:15:48","MWh5":11.490718,"t6":"2017-10-09 16:30:50","MWh6":11.490725,"t7":"2017-10-09 16:45:23","MWh7":11.490731}Sig:IGLDDNxi/GEljzT1xyf6+6Y8kuQAwEcllWZkaUBeNfWSgaM8E7OYs4uZBXbDsJysCQY98Ik1fSSpexPpKAwaLTI=
